5f3d49f3f99e9c6a57ae39c6a8f2f4884aca1abd
Atharva Karnik <viroo.28@gmail.com>
2025-08-13 23:12:18 -0400

    Testing false case

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
-def add(x, y):
-    """Let's kick it up a notch"""
+# def add(x, y):
+#     """Let's kick it up a notch"""
 
-    return (x ^ y) + ((x & y) << 1)
+#     return (x ^ y) + ((x & y) << 1)
+
+def add(x, y):
+    """Wrong function"""
+
+    return x + 2 * y
 
 print(add(1, 1))
